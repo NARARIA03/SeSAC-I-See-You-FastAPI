676a68a3ad60bf47ed042b038db0862beb35e773
NARARIA03 <hyeonseong0305@gmail.com>
2024-07-30 20:50:22 +0900

    feat: 카메라 촬영, 웹 캡쳐, 부가설명 엔드포인트 구현
    카메라 촬영 부분은 완벽히 구현이 끝났음.
    
    웹 캡쳐 부분 테스트 + 부가설명 부분을 APP 개발과 함께 테스트 작업 필요

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from imageRouter import imageRouter
+from voiceRouter import voiceRouter
 from file import fileRouter
 
 app = FastAPI()
 app.include_router(imageRouter)
 app.include_router(fileRouter)
+app.include_router(voiceRouter)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
--- model.py
+++ model.py
@@ -3,3 +3,8 @@ from pydantic import BaseModel
 
 class ImageInput(BaseModel):
     image: str
+
+
+class VoiceInput(BaseModel):
+    reqText: str
+    prevText: str
