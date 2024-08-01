04694fd1843a63a9aed6b1112834c68a069c3e98
NARARIA03 <hyeonseong0305@gmail.com>
2024-08-02 02:23:31 +0900

    화면 모드 구현 완료

--- imageRouter.py
+++ imageRouter.py
@@ -5,6 +5,11 @@ from gtts import gTTS
 from dotenv import load_dotenv
 import os
 from model import ImageInput
+import cv2
+import numpy as np
+import base64
+import io
+from PIL import Image
 
 load_dotenv()
 
@@ -18,6 +23,95 @@ def text_to_speech(text, filename):
     tts = gTTS(text=text, lang="ko")
     tts.save(filename)
     os.system(f"mpg321 {filename}")
+
+
+# base64 문자열을 이미지로 변환하는 함수
+def base64_to_image(base64_string):
+    img_data = base64.b64decode(base64_string)
+    image = Image.open(io.BytesIO(img_data))
+    return cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+
+
+# 이미지를 base64로 변환하는 함수
+def image_to_base64(image):
+    _, buffer = cv2.imencode(".png", cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
+    return base64.b64encode(buffer).decode("utf-8")
+
+
+# 릴루미노 모드 (저시력)
+def relumino_mode(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    equalized = cv2.equalizeHist(blurred)
+    adaptive_thresh = cv2.adaptiveThreshold(
+        equalized, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2
+    )
+    mask = adaptive_thresh == 255
+    output_image = np.zeros_like(image)
+    output_image[mask] = [0, 0, 0]
+    output_image[~mask] = image[~mask]
+    return output_image
+
+
+rgb2lms = np.array(
+    [
+        [0.31399022, 0.63951294, 0.04649755],
+        [0.15537241, 0.75789446, 0.08670142],
+        [0.01775239, 0.10944209, 0.87256922],
+    ]
+)
+lms_deuteranopia = np.array([[1, 0, 0], [0.494207, 0, 1.24827], [0, 0, 1]])
+lms2rgb = np.linalg.inv(rgb2lms)
+
+
+def rgb_to_lms(rgb):
+    return np.dot(rgb, rgb2lms.T)
+
+
+def simulate_deuteranopia(lms):
+    return np.dot(lms, lms_deuteranopia.T)
+
+
+def lms_to_rgb(lms):
+    return np.dot(lms, lms2rgb.T)
+
+
+def daltonize(image):
+    lms_image = rgb_to_lms(image)
+    lms_deuter_image = simulate_deuteranopia(lms_image)
+    deuter_image = lms_to_rgb(lms_deuter_image)
+    error = image - deuter_image
+    corrected_image = image + error * 0.2
+    return np.clip(corrected_image, 0, 1)
+
+
+# 적록색맹
+def process_image_base64(encoded_image) -> str:
+    image = base64_to_image(encoded_image)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) / 255.0
+    corrected_image = daltonize(image)
+    corrected_image_bgr = (corrected_image * 255).astype(np.uint8)
+    encoded_corrected_image = image_to_base64(corrected_image_bgr)
+
+    return encoded_corrected_image
+
+
+# 전색맹
+def gray_scale(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    equalized = cv2.equalizeHist(blurred)
+    adaptive_thresh = cv2.adaptiveThreshold(
+        equalized, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2
+    )
+    output_image = np.zeros_like(image)
+    output_image[:, :, 0] = gray
+    output_image[:, :, 1] = gray
+    output_image[:, :, 2] = gray
+    mask = adaptive_thresh == 255
+    output_image[mask] = [0, 0, 0]
+    output_image[~mask] = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)[~mask]
+    return output_image
 
 
 headers = {"Content-Type": "application/json", "Authorization": f"Bearer {API_KEY}"}
@@ -59,9 +153,24 @@ async def postCameraImage(imageInput: ImageInput) -> dict:
         return {"msg": response.status_code, "mp3": ""}
     else:
         try:
+            imgBase64 = ""
+            if imageInput.displayMode == "general":
+                imgBase64 = imageInput.image
+            elif imageInput.displayMode == "lowVision":
+                img = base64_to_image(imageInput.image)
+                adj = relumino_mode(img)
+                imgBase64 = image_to_base64(adj)
+            elif imageInput.displayMode == "redGreenColorBlind":
+                img = base64_to_image(imageInput.image)
+                imgBase64 = process_image_base64(img)
+            elif imageInput.displayMode == "totallyColorBlind":
+                img = base64_to_image(imageInput.image)
+                adj = gray_scale(img)
+                imgBase64 = image_to_base64(adj)
+
             content = response.json()["choices"][0]["message"]["content"]
             text_to_speech(content, f"./mp3/{fileName}.mp3")
-            return {"msg": content, "mp3": f"/mp3/{fileName}"}
+            return {"msg": content, "mp3": f"/mp3/{fileName}", "image": imgBase64}
         except KeyError as e:
             return {"msg": e, "mp3": ""}
 
@@ -131,8 +240,23 @@ async def postWebviewLowVision(imageInput: ImageInput) -> dict:
         return {"msg": response.status_code, "mp3": ""}
     else:
         try:
+            imgBase64 = ""
+            if imageInput.displayMode == "general":
+                imgBase64 = imageInput.image
+            elif imageInput.displayMode == "lowVision":
+                img = base64_to_image(imageInput.image)
+                adj = relumino_mode(img)
+                imgBase64 = image_to_base64(adj)
+            elif imageInput.displayMode == "redGreenColorBlind":
+                img = base64_to_image(imageInput.image)
+                imgBase64 = process_image_base64(img)
+            elif imageInput.displayMode == "totallyColorBlind":
+                img = base64_to_image(imageInput.image)
+                adj = gray_scale(img)
+                imgBase64 = image_to_base64(adj)
+
             content = response.json()["choices"][0]["message"]["content"]
             text_to_speech(content, f"./mp3/{fileName}.mp3")
-            return {"msg": content, "mp3": f"/mp3/{fileName}"}
+            return {"msg": content, "mp3": f"/mp3/{fileName}", "image": imgBase64}
         except KeyError as e:
             return {"msg": e, "mp3": ""}
