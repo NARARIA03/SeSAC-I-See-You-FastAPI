e44cb44cfd45edd877a33415912f01bb6898b031
NARARIA03 <hyeonseong0305@gmail.com>
2024-08-03 18:39:06 +0900

    fix: fileRouter 1개로 다시 롤백
    머지 과정에서 놓침

--- file.py
+++ file.py
@@ -4,13 +4,7 @@ from fastapi.responses import FileResponse
 fileRouter = APIRouter()
 
 
-@fileRouter.get("/mp3/image/{fileName}")
+@fileRouter.get("/mp3/{fileName}")
 async def getMp3File(fileName: int) -> FileResponse:
-    path = f"./mp3/image/{fileName}.mp3"
-    return FileResponse(path, media_type="audio/mpeg")
-
-
-@fileRouter.get("/mp3/voice/{fileName}")
-async def getMp3File(fileName: int) -> FileResponse:
-    path = f"./mp3/voice/{fileName}.mp3"
+    path = f"./mp3/{fileName}.mp3"
     return FileResponse(path, media_type="audio/mpeg")
